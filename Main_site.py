6e6e94cde4b4fefed7cea35b0ca8a463be38df2a
unknown <viniciusgarcia0412@gmail.com>
2026-07-13 21:15:31 -0300

    Atualizações

--- Main_site.py
+++ Main_site.py
@@ -26,7 +26,7 @@ GRAFICOS = BASE_DIR / "Resultados_Graficos"
 st.sidebar.title("Navegação")
 secao = st.sidebar.selectbox(
     "Menu:",
-    ["O projeto", "Falha no dielétrico", "Capacitância"]
+    ["O projeto", "Capacitância"]
 )
 
 #RENDERIZAÇÃO DAS ABAS
@@ -40,7 +40,7 @@ if secao == "O projeto":
     
     st.markdown("<br>", unsafe_allow_html=True) # Usado para gerar espaço entre os textos
 
-    st.markdown("""<p style="text-indent: 50px; text-align: justify;">Para tal, decidimos elaborar uma interface web que contenha os três trabalhos realizados em sob a geometria e propriedades de um dos objetos mais comuns e utilizados na engenharia, o cabo axial. </p>""", unsafe_allow_html=True)
+    st.markdown("""<p style="text-indent: 50px; text-align: justify;">Para tal, decidimos elaborar uma interface web que contenha os resultados de um trabalho realizado sobre a geometria e as propriedades de um dos objetos mais comuns e utilizados na engenharia, o cabo axial. </p>""", unsafe_allow_html=True)
     
     st.markdown("<br>", unsafe_allow_html=True) # Usado para gerar espaço entre os textos
 
@@ -59,39 +59,8 @@ if secao == "O projeto":
     
     st.markdown("<br>", unsafe_allow_html=True) # Usado para gerar espaço entre os textos
 
-    st.markdown("""<p style="text-indent: 50px; text-align: justify;">No painel lateral encontram-se as abas de navegação da interface, sendo trabalhado em cada uma um aspecto do cabo que um engenheiro deve se atentar na hora de criar o seu projeto. </p>""", unsafe_allow_html=True)
+    st.markdown("""<p style="text-indent: 50px; text-align: justify;">No painel lateral encontram-se as abas de navegação da interface, sendo trabalhado a questão da existência de um efeito capacitivo defeituoso na estrutura do cabo abordando diretamente uma questão que um engenheiro deve se atentar na hora de criar o seu projeto. </p>""", unsafe_allow_html=True)
 
-elif secao == "Falha no dielétrico":
-    st.header("Estudo do dielétrico do cabo coaxial") # Titulo da aba
-
-    # Texto da aba
-    st.markdown("""<p style="text-indent: 50px; text-align: justify;">Uma das maiores caracteristicas do cabo coaxial é a sua alta blindagem contra ondas eletromagnéticas do ambiente, contudo, com o passar dos anos e desgastes ocorridos por condições adversas o dielétrico desses cabos pode apresentar falhas e isso pode ocasionar problemas na sua aplicação. 
-                Devido a isso, foi-se elaborado um progrma que utiliza os dados fornecidos pelo professor, mapeia e plota em forma de 4 gráficos os resultados do que seria um estudo em campo do teste da integridade do dielétrico de um cabo coaxial.</p> """, unsafe_allow_html=True)
-    
-    st.markdown("<br>", unsafe_allow_html=True) # Usado para gerar espaço entre os textos
-
-    Col1, Col2, = st.columns(2) # Cria colunas para poder colocar cada imagem lado a lado
-    # Cada with define o que vai estar contido dentro de cada coluna 
-    with Col1: 
-        st.markdown("<p style='text-align: center;'>Imagem 1: Campo Vetorial da Densidade de Fluxo Elétrico (D)</p>", unsafe_allow_html=True) # Titulo da imagem
-        st.image(GRAFICOS / "Campo Vetorial da Densidade de Fluxo Elétrico (D).png") # Mostra a imagem
-        st.markdown("<p style='text-align: center;'>FONTE: Autoria própria (2026)</p>", unsafe_allow_html=True) # Fonte da imagem 
-    with Col2:
-        st.markdown("<p style='text-align: center;'>Imagem 2: Mapa de Calor - Divergente de D (Analítico)</p>", unsafe_allow_html=True)
-        st.image(GRAFICOS / "Mapa de Calor - Divergente de D (Analítico).png")
-        st.markdown("<p style='text-align: center;'>FONTE: Autoria própria (2026)</p>", unsafe_allow_html=True)
-
-    st.markdown("<br>", unsafe_allow_html=True) # Usado para gerar espaço entre os textos
-
-    Col3, Col4 = st.columns(2)
-    with Col3:
-        st.markdown("<p style='text-align: center;'>Imagem 3: Mapa de Calor - Divergente de D (Numérico)</p>", unsafe_allow_html=True)
-        st.image(GRAFICOS / "Mapa de Calor - Divergente de D (Numérico).png")
-        st.markdown("<p style='text-align: center;'>FONTE: Autoria própria (2026)</p>", unsafe_allow_html=True)
-    with Col4:
-        st.markdown("<p style='text-align: center;'>Imagem 4: Mapa de Calor - Erro absoluto</p>", unsafe_allow_html=True)
-        st.image(GRAFICOS / "Mapa de Calor - Erro absoluto.png")
-        st.markdown("<p style='text-align: center;'>FONTE: Autoria própria (2026)</p>", unsafe_allow_html=True)
 
 elif secao == "Capacitância":
     st.header("Estudo da capacitância do cabo coaxial") # Titulo da aba
